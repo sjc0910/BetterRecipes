506134bb8336b97387bc4fddf0492d08a6377675
sjc0910 <1826543852@qq.com>
2020-10-04 11:58:40 +0800

    添加黑曜石苹果

--- src/main/java/com/sjc0910/betterrecipes/ItemRegistry.java
+++ src/main/java/com/sjc0910/betterrecipes/ItemRegistry.java
@@ -9,5 +9,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
     public static final RegistryObject<Item> obsidianIngot = ITEMS.register("obsidian_ingot", ObsidianIngot::new);
+    public static final RegistryObject<Item> obsidianApple = ITEMS.register("obsidian_apple", ObsidianApple::new);
     public static final RegistryObject<Item> BRIcon = ITEMS.register("icon_of_better_recipes", BRIcon::new);
 }
---
+++ src/main/java/com/sjc0910/betterrecipes/ObsidianApple.java
@@ -0,0 +1,44 @@
+package com.sjc0910.betterrecipes;
+
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+
+import javax.swing.*;
+
+public class ObsidianApple extends Item {
+    public ObsidianApple(Properties properties) {
+        super(properties);
+    }
+    // 黑曜石苹果
+    // 恢复10饥饿值，10饱和度
+    // Buff:
+    // 1. 瞬间治疗 3
+    // 2. 血量上限 5 5m
+    // 3. 抗性提升 5 8m
+    // 4. 血量恢复 1 30s
+    // Debuff:
+    // 1. 中毒 1 3s 75%
+    // 2. 饥饿 2 30s 50%
+    // 3. 缓慢 3 20s
+    // 4. 失明 1 5s 80%
+    private static final Food food = (new Food.Builder())
+            .saturation(10)
+            .hunger(10)
+            .effect(()-> new EffectInstance(Effects.REGENERATION, 30 * 20, 1), 1)
+            .effect(()-> new EffectInstance(Effects.POISON, 3 * 20, 1), (float).75)
+            .effect(()-> new EffectInstance(Effects.INSTANT_HEALTH,1, 3), 1)
+            .effect(()-> new EffectInstance(Effects.HEALTH_BOOST, 5 * 60 * 20, 5), 1)
+            .effect(()-> new EffectInstance(Effects.HUNGER, 30 * 20, 2), (float).5)
+            .effect(()-> new EffectInstance(Effects.SLOWNESS, 20 * 20, 3), 1)
+            .effect(()-> new EffectInstance(Effects.INVISIBILITY, 5 * 20, 1), (float).8)
+            .effect(()-> new EffectInstance(Effects.STRENGTH, 8 * 60 * 20, 5), 1)
+            .build();
+
+    public ObsidianApple() {
+        super(new Properties().food(food).group(ModGroup.BRGroup));
+    }
+}
