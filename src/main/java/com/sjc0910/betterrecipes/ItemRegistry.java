306ac8e41e47c5b08363a7bebc8048e99df4ac16
sjc0910 <1826543852@qq.com>
2020-10-06 12:08:24 +0800

    添加钻石棍

--- src/main/java/com/sjc0910/betterrecipes/ItemRegistry.java
+++ src/main/java/com/sjc0910/betterrecipes/ItemRegistry.java
@@ -15,5 +15,6 @@ public class ItemRegistry {
     public static final RegistryObject<Item> obsidianApple = ITEMS.register("obsidian_apple", ObsidianApple::new);
     public static final RegistryObject<Item> obsidianSword = ITEMS.register("obsidian_sword", ObsidianSword::new);
     public static final RegistryObject<Item> obsidianPickaxe = ITEMS.register("obsidian_pickaxe", ObsidianPickaxe::new);
+    public static final RegistryObject<Item> diamondStick = ITEMS.register("diamond_stick", DiamondStick::new);
     public static final RegistryObject<Item> BRIcon = ITEMS.register("icon_of_better_recipes", BRIcon::new);
 }
