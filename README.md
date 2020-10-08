0a4b899270249b966a69b0702cb3ceef180aeeda
sjc0910 <1826543852@qq.com>
2020-10-08 19:40:23 +0800

    修复成吨的Bug，添加黑曜石锭的高炉配方

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Better Recipes
 Better Recipes  
-更好的合成表
-这个Mod将会增加一些实用的合成表    
+更好的合成表  
 
+这个Mod将会增加一些实用的合成表    
 
 ## 版本列表  
 
 |版本|说明|
 |---------------|--------------------------------|
-|[v2.1.1 (Latest)](https://github.com/sjc0910/BetterRecipes/releases/download/v2.1.1/BetterRecipes.jar)|添加钻石棍，完善镐和剑|
+|[v2.5.9 (Latest)](https://github.com/sjc0910/BetterRecipes/releases/download/v2.5.9/BetterRecipes.jar)|修复成吨的Bug，添加黑曜石锭的高炉配方|
+|[v2.1.1](https://github.com/sjc0910/BetterRecipes/releases/download/v2.1.1/BetterRecipes.jar)|添加钻石棍，完善镐和剑|
 |[v1.8.9](https://github.com/sjc0910/BetterRecipes/releases/download/v1.8.9/BetterRecipes.jar)|添加黑曜石锭、黑曜石剑合成配方及翻译文件|
 |v1.3.2|添加黑曜石苹果|
 |v0.0.1|配置好Gradle, Forge, MCP及各种配置文件|
--- src/main/java/com/sjc0910/betterrecipes/foods/ObsidianApple.java
+++ src/main/java/com/sjc0910/betterrecipes/foods/ObsidianApple.java
@@ -4,7 +4,7 @@ import com.sjc0910.betterrecipes.ModGroup;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.potion.Effect;
+import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
@@ -34,6 +34,8 @@ public class ObsidianApple extends Item {
             .effect(()-> new EffectInstance(Effects.SLOWNESS, 20 * 20, 3), 1)
             .effect(()-> new EffectInstance(Effects.INVISIBILITY, 5 * 20, 1), (float).8)
             .effect(()-> new EffectInstance(Effects.STRENGTH, 8 * 60 * 20, 5), 1)
+            .effect(()-> new EffectInstance(Effects.FIRE_RESISTANCE, 10 * 60 * 20, 1), 1)
+            .setAlwaysEdible()
             .build();
     public ObsidianApple() {
         super(new Properties().food(food).group(ItemGroup.FOOD));
